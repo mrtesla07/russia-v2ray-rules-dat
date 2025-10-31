7e6fedf6ed71b41a3eb7a882ee29c8cc6016da32
unknown <mrtesla07@gmail.com>
2025-11-01 02:42:26 +0300

    Add v2dat-lite merger and split geosite build

---
+++ tools/v2dat-lite/go.mod
@@ -0,0 +1,25 @@
+module github.com/mrtesla07/russia-v2ray-rules-dat/tools/v2dat-lite
+
+go 1.22
+
+require (
+	github.com/v2fly/v2ray-core/v5 v5.13.0
+	google.golang.org/protobuf v1.35.1
+)
+
+require (
+	github.com/adrg/xdg v0.4.0 // indirect
+	github.com/golang/protobuf v1.5.3 // indirect
+	github.com/kr/pretty v0.3.1 // indirect
+	github.com/pelletier/go-toml v1.9.5 // indirect
+	github.com/pires/go-proxyproto v0.7.0 // indirect
+	github.com/quic-go/quic-go v0.40.0 // indirect
+	go.starlark.net v0.0.0-20230612165344-9532f5667272 // indirect
+	go4.org/netipx v0.0.0-20230303233057-f1b76eb4bb35 // indirect
+	golang.org/x/crypto v0.16.0 // indirect
+	golang.org/x/net v0.19.0 // indirect
+	golang.org/x/sys v0.15.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20230822172742-b8732ec3820d // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
---
+++ tools/v2dat-lite/main.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"sort"
+
+	router "github.com/v2fly/v2ray-core/v5/app/router"
+	"google.golang.org/protobuf/proto"
+)
+
+func main() {
+	outputPath := flag.String("o", "", "output file path")
+	flag.Parse()
+
+	inputs := flag.Args()
+	if *outputPath == "" || len(inputs) == 0 {
+		log.Fatalf("usage: v2dat-lite -o output.dat input1.dat [input2.dat ...]")
+	}
+
+	merged := make(map[string]*router.GeoSite)
+	order := make([]string, 0, len(inputs))
+
+	for _, path := range inputs {
+		data, err := os.ReadFile(path)
+		if err != nil {
+			log.Fatalf("read %s: %v", path, err)
+		}
+
+		var list router.GeoSiteList
+		if err := proto.Unmarshal(data, &list); err != nil {
+			log.Fatalf("unmarshal %s: %v", path, err)
+		}
+
+		for _, site := range list.Entry {
+			cc := site.CountryCode
+			if existing, ok := merged[cc]; ok {
+				existing.Domain = append(existing.Domain, site.Domain...)
+			} else {
+				cloned := proto.Clone(site).(*router.GeoSite)
+				merged[cc] = cloned
+				order = append(order, cc)
+			}
+		}
+	}
+
+	sort.Strings(order)
+
+	result := &router.GeoSiteList{}
+	for _, cc := range order {
+		result.Entry = append(result.Entry, merged[cc])
+	}
+
+	out, err := proto.Marshal(result)
+	if err != nil {
+		log.Fatalf("marshal merged geosite: %v", err)
+	}
+
+	if err := os.WriteFile(*outputPath, out, 0o644); err != nil {
+		log.Fatalf("write %s: %v", *outputPath, err)
+	}
+
+	fmt.Fprintf(os.Stderr, "merged %d lists into %s\n", len(order), *outputPath)
+}
